dea784d660e1b42e5eec38da4b7788be860b69a5
Steven D <25520703+StevenDdrwrdr@users.noreply.github.com>
2022-09-09 04:45:34 -0700

    Added base controller module

---
+++ src/StarterPlayerScripts/class/control.lua
@@ -0,0 +1,21 @@
+--Preinitialization
+
+--Variables
+local UserInputService = game:GetService("UserInputService")
+
+local controller = {}
+--Initialization
+
+--Functions
+function controller:BindInput(deviceType, input, callback)
+	
+end
+
+function controller:UnbindInput(deviceType, input)
+
+end
+--Events
+
+--Post Initialization
+
+--Main
