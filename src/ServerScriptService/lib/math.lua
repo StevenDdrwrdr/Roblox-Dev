1c82e88660879435a223b430ca30ade2e709ea84
Steven D <25520703+StevenDdrwrdr@users.noreply.github.com>
2022-09-09 01:27:58 -0700

    Added base variables

--- src/ServerScriptService/lib/math.lua
+++ src/ServerScriptService/lib/math.lua
@@ -1,10 +1,16 @@
 --Initialization
 if not (script:IsA("ModuleScript")) then error(string.format("Failed to load %s %s", script.Name, script.ClassName)) end
 --Variables
-local m = {}
+local meta = {}
+local mathext = {}
+
+meta.__index = math
+meta.__newindex = function(t,k,v)
+    error("Unable to create value to math utility")
+end
 --Functions
 
 --Events
 
 --Main
-return m
+return setmetatable(mathext,meta)
