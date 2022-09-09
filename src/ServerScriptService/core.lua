ba1307e5ac5fb22857a34f2f33f6bdf17bdeea5e
Steven D <25520703+StevenDdrwrdr@users.noreply.github.com>
2022-09-09 01:31:06 -0700

    Changed comments

--- src/ServerScriptService/core.lua
+++ src/ServerScriptService/core.lua
@@ -1,9 +1,13 @@
---Initialization
+--Preinitialization
 if not (script:IsA("Script")) then error(string.format("Failed to load %s %s", script.Name, script.ClassName)) end
 --Variables
+
+--Initialization
 
 --Functions
 
 --Events
+
+--Postinitialization
 
 --Main
--- src/ServerScriptService/lib/math.lua
+++ src/ServerScriptService/lib/math.lua
@@ -1,9 +1,9 @@
---Initialization
+--Preinitialization
 if not (script:IsA("ModuleScript")) then error(string.format("Failed to load %s %s", script.Name, script.ClassName)) end
 --Variables
 local meta = {}
 local mathext = {}
-
+--Initialization
 meta.__index = math
 meta.__newindex = function(t,k,v)
     error("Unable to create value to math utility")
@@ -11,6 +11,8 @@ end
 --Functions
 
 --Events
+
+--Postinitialization
 
 --Main
 return setmetatable(mathext,meta)
